0445bb11bfff83c03969ee2fdbc83856c87b62f4
切图仔 <admin@oibit.cn>
2021-03-12 10:53:39 +0800

    feat: lint & add element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
