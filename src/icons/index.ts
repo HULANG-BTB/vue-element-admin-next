68d19ed0ff63078e34a26dbabf3656ca3a375f18
切图仔 <admin@oibit.cn>
2021-03-13 09:18:15 +0800

    feat: add icons

---
+++ src/icons/index.ts
@@ -0,0 +1,11 @@
+import SvgIcon from '@/components/svg-icon/index.vue'
+import { App } from 'vue'
+
+export default {
+  install: (app: App) => {
+    app.component(SvgIcon.name, SvgIcon)
+    const svgFiles = require.context('./svg', false, /\.svg$/)
+    const requireAll = (requireContext: any) => requireContext.keys().map(requireContext)
+    console.log(requireAll(svgFiles))
+  }
+}
