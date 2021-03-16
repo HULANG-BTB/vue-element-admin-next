2aafd0822e305b3798f36d5dd374f27df5547340
切图仔 <admin@oibit.cn>
2021-03-16 11:37:18 +0800

    feat: add setting views

--- src/layout/components/settings-view/index.vue
+++ src/layout/components/settings-view/index.vue
@@ -1,13 +1,115 @@
 <template>
-  <div></div>
+  <div class="drawer-container">
+    <div>
+      <h3 class="drawer-title">Page style setting</h3>
+
+      <!-- <div class="drawer-item">
+        <span>Theme Color</span>
+        <theme-picker style="float: right;height: 26px;margin: -3px 8px 0 0;" @change="themeChange" />
+      </div> -->
+
+      <div class="drawer-item">
+        <span>Open Tags-View</span>
+        <el-switch v-model="tagsView" class="drawer-switch" />
+      </div>
+
+      <div class="drawer-item">
+        <span>Fixed Header</span>
+        <el-switch v-model="fixedHeader" class="drawer-switch" />
+      </div>
+
+      <div class="drawer-item">
+        <span>Sidebar Logo</span>
+        <el-switch v-model="sidebarLogo" class="drawer-switch" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
+import { useStore } from 'vuex'
 
 export default defineComponent({
-  name: 'SettingsView'
+  name: 'SettingsView',
+  setup() {
+    const store = useStore()
+
+    const themeChange = (val: any) => {
+      store.dispatch('settings/changeSetting', {
+        key: 'theme',
+        value: val
+      })
+    }
+
+    const tagsView = computed({
+      get: () => {
+        return store.state.settings.tagsView
+      },
+      set: (val: any) => {
+        store.dispatch('settings/changeSetting', {
+          key: 'tagsView',
+          value: val
+        })
+      }
+    })
+
+    const fixedHeader = computed({
+      get: () => {
+        return store.state.settings.fixedHeader
+      },
+      set: (val: any) => {
+        store.dispatch('settings/changeSetting', {
+          key: 'fixedHeader',
+          value: val
+        })
+      }
+    })
+
+    const sidebarLogo = computed({
+      get: () => {
+        return store.state.settings.sidebarLogo
+      },
+      set: (val: any) => {
+        store.dispatch('settings/changeSetting', {
+          key: 'sidebarLogo',
+          value: val
+        })
+      }
+    })
+
+    return {
+      themeChange,
+      tagsView,
+      fixedHeader,
+      sidebarLogo
+    }
+  }
 })
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.drawer-container {
+  padding: 24px;
+  font-size: 14px;
+  line-height: 1.5;
+  word-wrap: break-word;
+
+  .drawer-title {
+    margin-bottom: 12px;
+    color: rgba(0, 0, 0, 0.85);
+    font-size: 14px;
+    line-height: 22px;
+  }
+
+  .drawer-item {
+    color: rgba(0, 0, 0, 0.65);
+    font-size: 14px;
+    padding: 12px 0;
+  }
+
+  .drawer-switch {
+    float: right;
+  }
+}
+</style>
