f59b5e67f1fcfd21b4310d72957a86cefbc4ed17
切图仔 <admin@oibit.cn>
2021-03-19 09:25:24 +0800

    feat: add 401 error page

--- src/router/index.ts
+++ src/router/index.ts
@@ -202,13 +202,13 @@ export const asyncRoutes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '401',
-        component: () => import('@/views/dashboard/index.vue'),
+        component: () => import('@/views/error-page/401.vue'),
         name: 'Page401',
         meta: { title: '401', noCache: true }
       },
       {
         path: '404',
-        component: () => import('@/views/dashboard/index.vue'),
+        component: () => import('@/views/error-page/404.vue'),
         name: 'Page404',
         meta: { title: '404', noCache: true }
       }
