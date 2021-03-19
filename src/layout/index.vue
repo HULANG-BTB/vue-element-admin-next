25c14f1fb7a1e79d605df5f0401d785a666cdfeb
切图仔 <admin@oibit.cn>
2021-03-19 14:36:55 +0800

    feat: fix layout error when scrollbar apperence

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,16 +2,18 @@
   <div :class="classObj" class="app-wrapper">
     <div v-if="device === 'mobile' && sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
     <side-bar class="sidebar-container" />
-    <div :class="{ hasTagsView: needTagsView }" class="main-container">
-      <div :class="{ 'fixed-header': fixedHeader }">
-        <nav-bar />
-        <tags-view v-if="needTagsView" />
+    <el-scrollbar class="scrollbar-wrapper">
+      <div :class="{ hasTagsView: needTagsView }" class="main-container">
+        <div :class="{ 'fixed-header': fixedHeader }">
+          <nav-bar />
+          <tags-view v-if="needTagsView" />
+        </div>
+        <app-main />
+        <right-panel v-if="showSettings">
+          <settings-view />
+        </right-panel>
       </div>
-      <app-main />
-      <right-panel v-if="showSettings">
-        <settings-view />
-      </right-panel>
-    </div>
+    </el-scrollbar>
   </div>
 </template>
 
@@ -81,6 +83,10 @@ export default defineComponent({
   position: relative;
   height: 100%;
   width: 100%;
+
+  .scrollbar-wrapper {
+    overflow-x: hidden !important;
+  }
 
   &.mobile.openSidebar {
     position: fixed;
