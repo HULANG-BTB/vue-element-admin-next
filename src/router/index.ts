9c8638ad2153722208e430410b4abe316060b97a
切图仔 <admin@oibit.cn>
2021-03-19 11:42:12 +0800

    feat: add table page route info

--- src/router/index.ts
+++ src/router/index.ts
@@ -179,6 +179,42 @@ export const asyncRoutes: Array<RouteRecordRaw> = [
     ]
   },
   {
+    path: '/table',
+    component: Layout,
+    redirect: '/table/complex-table',
+    name: 'Table',
+    meta: {
+      title: 'Table',
+      icon: 'table'
+    },
+    children: [
+      {
+        path: 'dynamic-table',
+        component: () => import('@/views/table/dynamic-table/index.vue'),
+        name: 'DynamicTable',
+        meta: { title: 'Dynamic Table' }
+      },
+      {
+        path: 'drag-table',
+        component: () => import('@/views/table/drag-table.vue'),
+        name: 'DragTable',
+        meta: { title: 'Drag Table' }
+      },
+      {
+        path: 'inline-edit-table',
+        component: () => import('@/views/table/inline-edit-table.vue'),
+        name: 'InlineEditTable',
+        meta: { title: 'Inline Edit' }
+      },
+      {
+        path: 'complex-table',
+        component: () => import('@/views/table/complex-table.vue'),
+        name: 'ComplexTable',
+        meta: { title: 'Complex Table' }
+      }
+    ]
+  },
+  {
     path: '/example',
     component: Layout,
     redirect: '/example/list',
