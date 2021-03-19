275bb3a946077b7cc23b174bae6207f73b20472e
切图仔 <admin@oibit.cn>
2021-03-19 11:19:05 +0800

    feat: add chart menus

--- src/router/index.ts
+++ src/router/index.ts
@@ -149,6 +149,36 @@ export const asyncRoutes: Array<RouteRecordRaw> = [
     ]
   },
   {
+    path: '/charts',
+    component: Layout,
+    redirect: 'noRedirect',
+    name: 'Charts',
+    meta: {
+      title: 'Charts',
+      icon: 'chart'
+    },
+    children: [
+      {
+        path: 'keyboard',
+        component: () => import('@/views/charts/keyboard.vue'),
+        name: 'KeyboardChart',
+        meta: { title: 'Keyboard Chart', noCache: true }
+      },
+      {
+        path: 'line',
+        component: () => import('@/views/charts/line.vue'),
+        name: 'LineChart',
+        meta: { title: 'Line Chart', noCache: true }
+      },
+      {
+        path: 'mix-chart',
+        component: () => import('@/views/charts/mix-chart.vue'),
+        name: 'MixChart',
+        meta: { title: 'Mix Chart', noCache: true }
+      }
+    ]
+  },
+  {
     path: '/example',
     component: Layout,
     redirect: '/example/list',
